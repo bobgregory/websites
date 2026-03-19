6f1bad52e6a4dae516532bb5a8b7461021d34a1c
bobgregory <hugo.the.5th@gmail.com>
2026-03-19 22:52:20 +0000

    new committe

--- webserver/websites/bangorcaving-club/committee.php
+++ webserver/websites/bangorcaving-club/committee.php
@@ -15,24 +15,49 @@
 
     <main>
         <div>
-            <h2>President - This delicious Pizza</h2>
-            <p1>mmh they taste so nice, stuffed crusts</p1>
-            <img src="sources/committee/president.png">
+            <h2>President - Luke Khulmann</h2>
+            <p1>More Khulmanns</p1>
+            <img src="sources/committee/president.png"   onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
         </div>          
         <div>
-            <h2>Secretary - A Cat</h2>
-            <p1>He seems to be a bit trapped in the currents</p1>
-            <img src="sources/committee/secretary.png">
+            <h2>Secretary - Janak Subberwal</h2>
+            <p1>Flood insurance for tents mandatory</p1>
+            <img src="sources/committee/secretary.png"  onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
         </div>
         <div>
-            <h2>Treasurer - Colins Christmas dinner</h2>
-            <p1>From our christmas meal at the BPC, mmh lovely</p1>
-            <img src="sources/committee/treasurer.png">
+            <h2>Treasurer - Osian Griffith</h2>
+            <p1>Flavour text goes here</p1>
+            <img src="sources/committee/treasurer.png"    onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
         </div>
         <div>
-            <h2>I will add in the new committe</h2>
-            <p1>Don't panic we aren't going to have a pizza in charge</p1>
-            <img src="sources/committee/thing.png">
+            <h2>Tackle Mistress - Sion Du</h2>
+            <p1>Go and wash your kit</p1>
+            <img src="sources/committee/tackle.png"    onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
+        </div>
+        <div>
+            <h2>Welfare - Ciara</h2>
+            <p1>Still Colins hairdresser?</p1>
+            <img src="sources/committee/welfare.png"    onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
+        </div>
+        <div>
+            <h2>Social Media - Ewan Gray</h2>
+            <p1>Nepo Caver from the red rose</p1>
+            <img src="sources/committee/media.png"    onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
+        </div>
+        <div>
+            <h2>Whimsy - Elias Curtis-Monro</h2>
+            <p1>Go to kebaby, bring mozzerella sticks, for your whimsical leader</p1>
+            <img src="sources/committee/social.png"    onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
+        </div>
+        <div>
+            <h2>Training - Janak Subberal</h2>
+            <p1>Underground, overground wombling free</p1>
+            <img src="sources/committee/training.png"    onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
+        </div>
+        <div>
+            <h2>Webmaster - James Gregory</h2>
+            <p1>Please send in photos and flavour texts/little bios</p1>
+            <img src="sources/committee/web.png"    onerror="if (this.src != 'sources/committee/blank.png') this.src = 'sources/committee/blank.png';">
         </div>
     </main>
 
