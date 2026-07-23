97b5045e53f9fb17126ece6ac713d5d2eaf1548b
bobgregory <hugo.the.5th@gmail.com>
2026-07-23 17:41:47 +0100

    Join page added QRs and reworked

--- webserver/websites/bangorcaving-club/join.php
+++ webserver/websites/bangorcaving-club/join.php
@@ -17,32 +17,76 @@
     <main>
 
         <div>
+            <h2>How to Join</h2>
+            <p1>
+                The club is open to all people student or not. No membership is required for your first 2 trips. <br>
+                Day trips and away trips are open to beginners!<br>
+                Sign up forms can be found in the places listed below.<br><br>
+
+                A guide to your first away trip<br>
+                A guide to your first day trip
+            </p1>
+        </div>
+
+        <div>
             <h2>Socials</h2>
             <p1>
-                If you are interested in joining the club, or want to give caving a go come to a social, have a pint<small>(alcohol not required)</small> have a chat.<br>
-                 Every* thursday at 8pm in the snug at <a href="https://maps.app.goo.gl/bYBH5qE11ajM8F3N7">The Harp Inn</a>
+                Come to our socials, have a pint<small>(alcohol not required)</small> have a chat.<br>
+                 Every* thursday at 8pm in the snug** at <a href="https://maps.app.goo.gl/bYBH5qE11ajM8F3N7">The Harp Inn</a>
                 <br>
                 <br>
-                * <small>Socials occuring during university term time, occasionally on reading weeks and other holidays often not</small><br>
+                * <small>Socials occuring during university term time, occasionally on reading weeks and other holidays, often not</small><br>
+                **<small>The snug is the room to the right of the bar</small>
             </p1>
         </div>
 
-        <div>
-            <h2>Join the Whatsapp Group</h2>
-            <p1>
-                The Whatsapp group is the main way we communicate with members, and is where you will find out about trips and socials.
-                <a href="https://chat.whatsapp.com/I81KRzpXyG6FQOOCMQyDni"><img src="sources/qrs/whatsapp.png"/></a>
-            </p1>
+        <div class="horizontal-align">
+            <div><h2>WhatsApp Group</h2>
+                <p1>
+                Easiest place to find sign up forms, information about socials, and any other club activities
+                <br>
+                <br> QR code is clickable!
+                </p1>
+            </div> 
+            <a href="https://chat.whatsapp.com/I81KRzpXyG6FQOOCMQyDni" class="qr">
+                <img src="sources/qrs/whatsapp.png" class="qr-img">
+                <div class="insert-container">
+                    <img src="sources/qrs/whatsappinsert.png" class="qr-insert">
+                </div>
+            </a>
         </div>
 
-        <div>
-            <h2>We have an instagram</h2>
-            <p1>
-                go look at our instagram, it has things on it<br>
-            </p1>
+        <div class="horizontal-align">
+            <div><h2>Instagram</h2>
+                <p1>
+                The second easiest place to find sign up forms, information about socials, and any other club activities
+                <br>
+                <br> QR code is clickable!
+                </p1>
+            </div> 
+            <a href="https://www.instagram.com/bangorcaving" class="qr">
+                <img src="sources/qrs/instamain.png" class="qr-img">
+                <div class="insert-container">
+                    <img src="sources/qrs/instainsert.png" class="qr-insert">
+                </div>
+            </a>
+        </div>
 
-            
-        </div> 
+        <div class="horizontal-align">
+            <div><h2>The calendar</h2>
+                <p1>
+                All event dates will be in this calendar, you can add it to your calendar using this link!
+                <br>
+                <br> QR code is clickable!
+                </p1>
+            </div> 
+            <a href="https://calendar.google.com/calendar/u/1?cid=YmFuZ29ydW5pY2F2aW5nQGdtYWlsLmNvbQ" class="qr">
+                <img src="sources/qrs/calendar.png" class="qr-img">
+                <div class="insert-container">
+                    <img src="sources/qrs/calinsert.png" class="qr-insert">
+                </div>
+            </a>
+        </div>
     </main>
 
     <?php include 'footer.php';?>
